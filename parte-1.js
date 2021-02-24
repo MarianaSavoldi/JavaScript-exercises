b518219f9fc300574ef2d79a8d04bd44a8960a52
Mariana Savoldi <marianasavodi123@gmail.com>
2021-02-24 15:42:59 -0300

    Exercicio da parte 1 do primeiro dia do bloco 4

---
+++ parte-1.js
@@ -0,0 +1,8 @@
+const name = "Mariana Savoldi Pereira";
+const birthCity = "Jacareí - SP";
+let birthYear = 2000;
+birthYear = 2020;
+// birthCity = "São Paulo"; ocorre um erro, pois não pode se alterar o valor de uma contante.
+console.log(name);
+console.log(birthCity);
+console.log(birthYear);
