3fbcb4b6c06f5781791fe49f6d810b469cdf4788
Mariana Savoldi <marianasavodi123@gmail.com>
2021-02-24 16:11:21 -0300

    Exercicio parte 2

---
+++ parte-2.js
@@ -0,0 +1,8 @@
+let base = 5;
+let altura = 8;
+console.log(base);
+console.log(altura);
+let area = base * altura;
+console.log(area);
+let perimetro = base + altura + base + altura;
+console.log(perimetro);
